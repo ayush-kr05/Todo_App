48b7a424fc0f32ead5156f4fd83ac55254906f39
ayush-kr05 <ayushkumar.barbighaiya@gmail.com>
2022-08-18 14:07:12 +0530

    Todo_App

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
-document.querySelector("form").addEventListener("submit",addToDo);
+document.querySelector("form").addEventListener("submit", addToDo);
 
-function addToDo(){
-    event.preventDefault();
+function addToDo() {
+  event.preventDefault();
 
-    var task = document.querySelector("#task").value;
-    var priority = document.querySelector("#priority").value;
-    console.log(task,priority);
+  var task = document.querySelector("#task").value;
+  var priority = document.querySelector("#priority").value;
+  console.log(task, priority);
 
-    var tr = document.createElement("tr");
-    var td1 = document.createElement("td");
-    td1.innerText = task;
+  var tr = document.createElement("tr");
+  var td1 = document.createElement("td");
+  td1.innerText = task;
 
-    var td2 = document.createElement("td");
-    td2.innerText=priority;
-    if(priority=="High"){
-        td2.style.backgroundColor="red";
-    }else{
-        td2.style.backgroundColor="green";
-    }
+  var td2 = document.createElement("td");
+  td2.innerText = priority;
+  if (priority == "High") {
+    td2.style.backgroundColor = "red";
+  } else {
+    td2.style.backgroundColor = "green";
+  }
 
-    var td3=document.createElement("td");
-    td3.innerText="Delete";
-    td3.style.color="red";
-    td3.addEventListener("click",deleteRow);
+  var td3 = document.createElement("td");
+  td3.innerText = "Delete";
+  td3.style.color = "red";
+  td3.addEventListener("click", deleteRow);
 
-    tr.append(td1,td2,td3);
+  tr.append(td1, td2, td3);
 
-    document.querySelector("tbody").append(tr);
-
+  document.querySelector("tbody").append(tr);
 }
 
-function deleteRow(){
-   
-    event.target.parentNode.remove();
+function deleteRow() {
+  event.target.parentNode.remove();
 }
-
-
-
